c58adc623a5eb1d788cd13beb7e96ec3dbbe536d
Julius170 <juliusyoola5@gmail.com>
2022-09-16 13:49:39 +0100

    ERPN

--- Evaluate_Reverse_Polish_Notation.py
+++ Evaluate_Reverse_Polish_Notation.py
@@ -8,8 +8,14 @@ tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"] # answer 22
 
 
 # Solution 
+# Using a stack:
+
 stack = []
+# Loping through every charaters in the token array:
 for i in tokens:
+
+# If the character is an operator: remove the last 2 characters as operands and use the operatior on the operands,
+# and append the resultto the stack
     if i == "+" :
         x = int(stack.pop())
         y = int(stack.pop())
@@ -30,6 +36,7 @@ for i in tokens:
         y = int(stack.pop())
         print(int(y/x))
         stack.append(int(y / x))
+# Otherwise add the current chracter to the stack
     elif i.isnumeric() or type(int(i) == int):
         stack.append(i)
 
