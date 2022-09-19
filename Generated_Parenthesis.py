78d7aed37342253d5719252b4d67af6947b3ea55
Julius170 <juliusyoola5@gmail.com>
2022-09-19 02:38:08 +0100

    Generate Parenthesis

--- Generated_Parenthesis.py
+++ Generated_Parenthesis.py
@@ -1,9 +1,34 @@
+# Question 
 # Given n pairs of parentheses, 
 # write a function to generate all combinations of well-formed parentheses.
 
-# n = 3
-# arr = []
-# for i in range(1,n+1):
-    
 
-# print(arr)
+
+    # Solution
+def gen_Parenthesis(n):
+    stack = []
+    res = []
+
+    def backtrack(openN, closeN):
+        if openN == closeN == n:
+            res.append("".join(stack))
+            return
+
+        if openN < n:
+            stack.append("(")
+            backtrack(openN+1, closeN)
+            stack.pop()
+
+        if closeN < openN:
+            stack.append(")")
+            backtrack(openN, closeN+1)
+            stack.pop()
+        
+    backtrack(0, 0)
+    return res
+
+
+
+# Test Cases
+n= 3
+print(gen_Parenthesis(n))
